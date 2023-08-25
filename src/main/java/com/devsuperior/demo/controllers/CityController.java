7faa699604ed6531391b5d84882533c8e5185227
taohansens <taohansens@gmail.com>
2023-08-25 18:13:50 -0300

    Boilerplate ok

---
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -0,0 +1,22 @@
+package com.devsuperior.demo.controllers;
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+	@Autowired
+	private CityService service;
+	
+	@GetMapping
+	public ResponseEntity<List<CityDTO>> findAll() {
+		List<CityDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	public List<CityDTO> findAll() {
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(CityDTO::new).toList();
+	}
+}
